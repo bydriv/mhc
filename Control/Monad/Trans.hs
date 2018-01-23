8f36d5a52d00d70309655fb36269e5bf4d1601ef
bydriv <bydriv@gmail.com>
2018-01-23 23:21:43 +0900

    Add Control/Monad/Trans.hs

---
+++ Control/Monad/Trans.hs
@@ -0,0 +1,19 @@
+-- MHC: Minimal Haskell Compiler © 2017 Kaoru Kawamukai <bydriv@gmail.com>
+--
+-- MHC is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- MHC is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with MHC.  If not, see <http://www.gnu.org/licenses/>.
+
+module Control.Monad.Trans (MonadTrans (lift)) where
+
+class MonadTrans t where
+  lift :: Monad m => m a -> t m a
