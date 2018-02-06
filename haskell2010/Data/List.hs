e650e67f2476d04a76ec5eaad1015667e0c66164
bydriv <bydriv@gmail.com>
2018-02-07 01:21:49 +0900

    Add functions to Data.List

--- haskell2010/Data/List.hs
+++ haskell2010/Data/List.hs
@@ -13,10 +13,30 @@
 -- You should have received a copy of the GNU General Public License
 -- along with MHC.  If not, see <http://www.gnu.org/licenses/>.
 
-{-# LANGUAGE NoImplicitPrelude #-}
-
 module Data.List where {
   (++) :: [a] -> [a] -> [a];
   [] ++ l = l;
   (x : xs) ++ l = x : (xs ++ l);
+
+  head :: [a] -> a;
+  head (x : _) = x;
+
+  last :: [a] -> a;
+  last [x] = x;
+  last (_ : xs) = last xs;
+
+  tail :: [a] -> [a];
+  tail (_ : xs) = xs;
+
+  init :: [a] -> [a];
+  init [x] = [];
+  init (x : xs) = x : init xs;
+
+  null :: [a] -> Bool;
+  null [] = True;
+  null (_ : _) = False;
+
+  length :: [a] -> Int;
+  length [] = 0;
+  length (_ : xs) = length xs + 1;
 }
