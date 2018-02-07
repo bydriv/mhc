e5a6fdf56d463622e4ec23d96d5190ccbedbb17f
bydriv <bydriv@gmail.com>
2018-02-07 19:00:38 +0900

    Emulate parse-error(t)

--- Language/Haskell2010/Layout.hs
+++ Language/Haskell2010/Layout.hs
@@ -5,9 +5,18 @@ import qualified Data.Char                    as Char
 import qualified Language.Haskell2010.Lexing  as Lexing
 import qualified Language.Haskell2010.Parsing as Parsing
 
+data LayoutContext =
+    Unknown
+  | Module
+  | Let
+  | Where
+  | Do
+  | Of
+  deriving (Eq, Ord, Show, Read)
+
 data Token =
     Zero
-  | Brace Int
+  | Brace LayoutContext Int
   | Chevron Int
   | Token Parsing.Token
   deriving (Eq, Ord, Show, Read)
@@ -49,7 +58,7 @@ preprocessMod (Right token : tokens) = do
   let (_, m) = Parsing.posOf token
   State.modify (\(i, first) -> (i + m, False))
   tokens' <- preprocess' tokens
-  return $ Brace n : Token token : tokens'
+  return $ Brace Module n : Token token : tokens'
 
 preprocess' :: [Either (Parsing.Pos, String) Parsing.Token] -> State.State (Int, Bool) [Token]
 preprocess' [] = return []
@@ -67,28 +76,28 @@ preprocess' (Right token : tokens) = do
   case token of
     Parsing.LET _ ->
       if isFirst then do
-        tokens' <- preprocessKw token tokens
+        tokens' <- preprocessKw Let token tokens
         return $ Chevron m : tokens'
       else
-        preprocessKw token tokens
+        preprocessKw Let token tokens
     Parsing.WHERE _ ->
       if isFirst then do
-        tokens' <- preprocessKw token tokens
+        tokens' <- preprocessKw Where token tokens
         return $ Chevron m : tokens'
       else
-        preprocessKw token tokens
+        preprocessKw Where token tokens
     Parsing.DO _ ->
       if isFirst then do
-        tokens' <- preprocessKw token tokens
+        tokens' <- preprocessKw Do token tokens
         return $ Chevron m : tokens'
       else
-        preprocessKw token tokens
+        preprocessKw Do token tokens
     Parsing.OF _ ->
       if isFirst then do
-        tokens' <- preprocessKw token tokens
+        tokens' <- preprocessKw Of token tokens
         return $ Chevron m : tokens'
       else
-        preprocessKw token tokens
+        preprocessKw Of token tokens
     _ ->
       if isFirst then do
         tokens' <- preprocess' tokens
@@ -97,13 +106,13 @@ preprocess' (Right token : tokens) = do
         tokens' <- preprocess' tokens
         return $ Token token : tokens'
 
-preprocessKw :: Parsing.Token -> [Either (Parsing.Pos, String) Parsing.Token] -> State.State (Int, Bool) [Token]
-preprocessKw kw [] = return [Token kw, Zero]
-preprocessKw kw (Left (_, "\n") : tokens) = do { State.put (0, True); preprocessKw kw tokens }
-preprocessKw kw (Left (_, "\r") : tokens) = do { State.put (0, True); preprocessKw kw tokens }
-preprocessKw kw (Left (_, "\n\r") : tokens) = do { State.put (0, True); preprocessKw kw tokens }
-preprocessKw kw (Left (_, sp) : tokens) = do { State.modify (\(i, first) -> (i + calcWidth sp, first)); preprocessKw kw tokens }
-preprocessKw kw (Right token : tokens) =
+preprocessKw :: LayoutContext -> Parsing.Token -> [Either (Parsing.Pos, String) Parsing.Token] -> State.State (Int, Bool) [Token]
+preprocessKw cxt kw [] = return [Token kw, Zero]
+preprocessKw cxt kw (Left (_, "\n") : tokens) = do { State.put (0, True); preprocessKw cxt kw tokens }
+preprocessKw cxt kw (Left (_, "\r") : tokens) = do { State.put (0, True); preprocessKw cxt kw tokens }
+preprocessKw cxt kw (Left (_, "\n\r") : tokens) = do { State.put (0, True); preprocessKw cxt kw tokens }
+preprocessKw cxt kw (Left (_, sp) : tokens) = do { State.modify (\(i, first) -> (i + calcWidth sp, first)); preprocessKw cxt kw tokens }
+preprocessKw cxt kw (Right token : tokens) =
   case token of
     Parsing.LBRACE _ -> do
       tokens' <- preprocess' $ Right token : tokens
@@ -111,33 +120,54 @@ preprocessKw kw (Right token : tokens) =
     _ -> do
       n <- State.gets fst
       tokens' <- preprocess' $ Right token : tokens
-      return $ Token kw : Brace n : tokens'
+      return $ Token kw : Brace cxt n : tokens'
 
-layout :: [Token] -> [Int] -> Parsing.Pos -> [Parsing.Token]
-layout (Chevron n : ts) (m : ms) pos
-  | m == n = Parsing.SEMICOLON pos : layout ts (m : ms) pos
+layout :: [Token] -> [(LayoutContext, Int)] -> Parsing.Pos -> [Parsing.Token]
+layout (Chevron n : ts) ((mcxt, m) : ms) pos
+  | m == n = Parsing.SEMICOLON pos : layout ts ((mcxt, m) : ms) pos
   | n < m = Parsing.RBRACE pos : layout (Chevron n : ts) ms pos
 layout (Chevron n : ts) ms pos =
   layout ts ms pos
-layout (Brace n : ts) (m : ms) pos
-  | n > m = Parsing.LBRACE pos : layout ts (n : m : ms) pos
-layout (Brace n : ts) [] pos
-  | n > -1 = Parsing.LBRACE pos : layout ts [n] pos
-layout (Brace n : ts) ms pos =
+layout (Brace ncxt n : ts) ((mcxt, m) : ms) pos
+  | n > m = Parsing.LBRACE pos : layout ts ((ncxt, n) : (mcxt, m) : ms) pos
+layout (Brace ncxt n : ts) [] pos
+  | n > -1 = Parsing.LBRACE pos : layout ts [(ncxt, n)] pos
+layout (Brace _ n : ts) ms pos =
   Parsing.LBRACE pos : Parsing.RBRACE pos : layout (Chevron n : ts) ms pos
-layout (Token token@(Parsing.RBRACE _) : ts) (-1 : ms) _ =
+layout (Token token@(Parsing.RBRACE _) : ts) ((_, -1) : ms) _ =
   let pos = Parsing.posOf token in
     Parsing.RBRACE pos : layout ts ms pos
 layout (Token token@(Parsing.RBRACE _) : ts) ms _ =
   undefined
 layout (Token token@(Parsing.LBRACE _) : ts) ms _ =
   let pos = Parsing.posOf token in
-    Parsing.LBRACE pos : layout ts (-1 : ms) pos
-layout (Token token : ts) (m : ms) pos
-  | m /= -1 && parseError token = Parsing.RBRACE pos : token : layout ts ms pos
+    Parsing.LBRACE pos : layout ts ((Unknown, -1) : ms) pos
+layout (Token token : ts) ((mcxt, m) : ms) pos
+  | m /= -1 && isIn token =
+      let n = countDropLet ((mcxt, m) : ms) in
+      let ms' = drop n ((mcxt, m) : ms) in
+        replicate n (Parsing.RBRACE pos) ++ (token : layout ts ms' pos)
+  | m /= -1 && isWhere token =
+      let n = countDropWhere ((mcxt, m) : ms) in
+      let ms' = drop n ((mcxt, m) : ms) in
+        replicate n (Parsing.RBRACE pos) ++ (token : layout ts ms' pos)
   where
-    parseError (Parsing.IN _) = True
-    parseError _ = False
+    isIn (Parsing.IN _) = True
+    isIn _ = False
+
+    countDropLet [] = 0
+    countDropLet ((Unknown, _) : _) = 0
+    countDropLet ((Let, _) : _) = 1
+    countDropLet (_ : stack) = countDropLet stack + 1
+
+    isWhere (Parsing.WHERE _) = True
+    isWhere _ = False
+
+    countDropWhere [] = 0
+    countDropWhere ((Unknown, _) : _) = 0
+    countDropWhere ((Module, _) : _) = 0
+    countDropWhere ((Where, _) : _) = 0
+    countDropWhere (_ : stack) = countDropWhere stack + 1
 layout (Token token : ts) ms _ =
   token : (layout ts ms (Parsing.posOf token))
 layout [] [] _ =
